bbf13b3fd8555b8f8e333bd850fe3fda7ab3c1d8
Noylah <pucci8047@gmail.com>
2026-09-23 20:35:15 +0200

    fix: error handling (ValueError)

--- input.py
+++ input.py
@@ -14,12 +14,21 @@ def input_matrice():
         console.print("[red][Errore] Inserisci i valori separati da uno spazio.")
         sys.exit(1)
 
-    riga1 = [int(x) for x in input1.split()]   
+    try:
+        riga1 = [int(x) for x in input1.split()]   
+    except ValueError:
+        console.print("[red][Errore] I valori non sono validi, inserisci numeri interi.")
+        sys.exit(1)
+
     matrice.append(riga1)
 
     for i in range(1, len(riga1)):
         riga_input = console.input(f"[magenta]Riga {i+1}: [/]")
-        riga = [int(x) for x in riga_input.split()]
+        try:
+            riga = [int(x) for x in riga_input.split()]
+        except ValueError:
+            console.print("[red][Errore] I valori non sono validi, inserisci numeri interi.")
+            sys.exit(1)
         if not riga:
             console.print("[red][Errore] Inserisci i valori separati da uno spazio.")
             sys.exit(1)
